14d6446bcff67f0d575f5d40a4bfbc3a599137eb
sakaki <s.s.km@softbank.ne.jp>
2021-04-05 15:18:27 +0900

    hug

--- app.py
+++ app.py
@@ -9,9 +9,19 @@ def base():
     return render_template('base.html')
 
 
-@app.route("/base.html")
+@app.route('/login', methods=["GET"])
 def login():
     return render_template('login.html')
+
+
+@app.route('/kifu', methods=["GET"])
+def kifu():
+    return render_template('kifu.html')
+
+
+# @app.route('/page_not_found', methods=["GET"])
+# def page_not_found():
+    # return render_template('/page_not_found.html')
 
 
 if __name__ == "__main__":
