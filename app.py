e252654ebfdb4048e7e2a2a33a7a4d9eb1297642
sakaki <s.s.km@softbank.ne.jp>
2021-04-07 02:40:18 +0900

    hug3

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
@@ -19,10 +19,34 @@ def kifu():
     return render_template('kifu.html')
 
 
-@app.route('/page_not_found', methods=["GET", "POST"])
-def page_not_found():
-    return render_template('/page_not_found.html')
+# @app.route("/login", methods=["GET"])
+# def login_get():
+    # if "user_id" in session:
+    # return render_template("login.html")
+    # else:  # ログインに飛ばす
+    # return redirect('/login')
 
+
+@app.route("/kifu", methods=["POST"])
+def kifu_post():
+    # 入力ホーム
+    name = request.form.get("name")
+    address = request.form.get("address")
+    email = request.form.get("email")
+    remark = request.form.get("remark")
+    conn = sqlite3.connect("Hugtest.db")
+    c = conn.cursor()
+    c.execute
+    ("insert into users values(null,?,?,?,?)", (name, address, email, remark))
+    conn.commit()
+    conn.close()
+    return "ok"
+    # return redirect("/2page_not_found.html")  # 関数を飛ばしたい時リダイレクトを
+
+
+# @app.route('/page_not_found', methods=["GET",])
+# def page_not_found():
+    # return render_template('/page_not_found.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
